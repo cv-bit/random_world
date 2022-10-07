c9e51885201c958263730db4df6b0e148407c439
cv-bit <ververcameron29@gmail.com>
2022-10-06 21:19:29 -0500

    upload random locations and fights

--- script.js
+++ script.js
@@ -11,13 +11,17 @@ const worldX = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 
 const worldY = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 
-const color_1 = '#00FF00'
+const locationNum = Math.floor((Math.random() * 5) +1);
+const enemyNum = Math.floor((Math.random() * 10) +1);
+
+
+const color_1 = 'rgb(0, 255, 0)'
 const color_2 = '#0000FF'
 const color_3 = '#CD853F'
 
 const color_4 = '#484848'
-const color_5 = '#888888'
-const color_6 = ''
+const color_5 = 'rgb(136, 136, 136)'
+const color_6 = '#000'
 
 map.style.backgroundColor = color_3
 game.style.backgroundColor = color_2
@@ -61,20 +65,20 @@ const shuffle = (arr) => {
     let curId = arr.length;
     // There remain elements to shuffle
     while (0 !== curId) {
-      // Pick a remaining element
-      let randId = Math.floor(Math.random() * curId);
-      curId -= 1;
-      // Swap it with the current element.
-      let tmp = arr[curId];
-      arr[curId] = arr[randId];
-      arr[randId] = tmp;
+        // Pick a remaining element
+        let randId = Math.floor(Math.random() * curId);
+        curId -= 1;
+        // Swap it with the current element.
+        let tmp = arr[curId];
+        arr[curId] = arr[randId];
+        arr[randId] = tmp;
     }
-    console.log(arr)
     return arr;
 }
 
 const randomWorld = () => {
     let allBlocks = []
+    let path = []
     for (let i = 1; i < (worldX.length * worldY.length) + 1; i++) {
         allBlocks.push(i)
     }
@@ -83,12 +87,28 @@ const randomWorld = () => {
     for (let i = 0; i < randomBlocks.length; i++) {
         let currBlock = document.getElementById(randomBlocks[i])
         currBlock.style.backgroundColor = shuffle(world_tiles)[0]
+        
+        if (currBlock.style.backgroundColor === color_1) {
+            path.push(currBlock)
+        }
+    }
+    
+    for (let i = 0; i < locationNum; i++) {
+        let randomLocation = shuffle(path)
+        randomLocation[i].style.backgroundImage = 'url(./assets/imgs/icons8-star-64.png)'
+        randomLocation[i].style.backgroundSize = 'contain'
+    }
 
+    for (let i = 0; i < enemyNum; i++) {
+        let randomLocation = shuffle(path)
+        randomLocation[i].style.backgroundImage = 'url(./assets/imgs/icons8-swords-64.png)'
+        randomLocation[i].style.backgroundSize = 'contain'
     }
 }
 
 const randomDungeon = () => {
     let allBlocks = []
+    let path = []
     for (let i = 101; i < ((worldX.length) * worldY.length) + 101; i++) {
         allBlocks.push(i)
     }
@@ -97,7 +117,25 @@ const randomDungeon = () => {
     for (let i = 0; i < randomBlocks.length; i++) {
         let currBlock = document.getElementById(randomBlocks[i])
         currBlock.style.backgroundColor = shuffle(dungeon_tiles)[0]
+        if (currBlock.style.backgroundColor === color_5) {
+            path.push(currBlock)
+        }
 
+        if (currBlock.style.backgroundColor === color_5) {
+            path.push(currBlock)
+        }
+    }
+
+    for (let i = 0; i < locationNum; i++) {
+        let randomLocation = shuffle(path)
+        randomLocation[i].style.backgroundImage = 'url(./assets/imgs/icons8-star-64.png)'
+        randomLocation[i].style.backgroundSize = 'contain'
+    }
+
+    for (let i = 0; i < enemyNum; i++) {
+        let randomLocation = shuffle(path)
+        randomLocation[i].style.backgroundImage = 'url(./assets/imgs/icons8-swords-64.png)'
+        randomLocation[i].style.backgroundSize = 'contain'
     }
 }
 
